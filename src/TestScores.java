dfbf910c61f1683167e93817acd6da912f149799
chambersl <league@wts22.attlocal.net>
2018-12-11 16:56:32 -0800

    First day of mod 4

---
+++ src/TestScores.java
@@ -0,0 +1,15 @@
+package doubles_and_booleans;
+
+import javax.swing.JOptionPane;
+
+public class TestScores {
+	public static void main(String[] args) {
+		String score = JOptionPane.showInputDialog("What did you get on your test?");
+		double percent = Double.parseDouble(score);
+		if (percent >= 70) {
+			JOptionPane.showMessageDialog(null, "Great Job! You passed!");
+		} else {
+			JOptionPane.showMessageDialog(null, "Better luck next time!");
+		}
+	}
+}
